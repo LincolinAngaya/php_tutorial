1ef0459c8d456fa0c1a6c173f0294ad73b9b60f3
Lincolin Angaya <lincolinangaya5626@gmail.com>
2024-12-18 11:13:51 -0800

    learning p[hp arrays

--- hello.php
+++ hello.php
@@ -4,5 +4,9 @@ $variable_name = 'hello, world';
 $number = 22/2;
 echo $variable_name;
 echo $number;
+echo 'my age is $number';
 
+// arrays
+$conctact = array('name', 'email', 'address');
+print_r($conctact);
 ?>
