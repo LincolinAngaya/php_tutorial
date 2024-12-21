c6a802f6d9770836e7d4010874f96a7fd19ba8f1
Lincolin Angaya <lincolinangaya5626@gmail.com>
2024-12-21 09:10:16 -0800

    done learning globalvariables

--- superglobal.php
+++ superglobal.php
@@ -9,11 +9,38 @@ $_FILES
 $_ENV
 $_COOKIE
 $_SESSION -->
-<?php
-$x = 75;
-function myfunction (){
-    echo $GLOBALS['x'];
-}
-myfunction();
+<html>
+<body>
 
+<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+  Name: <input type="text" name="fname">
+  <input type="submit">
+</form>
+<?php
+// $x = 75;
+// function myfunction (){
+//     echo $GLOBALS['x'];
+// }
+// myfunction();
+
+// $_SERVER is a PHP super global variable which holds information about headers, paths, and script locations.
+
+// echo $_SERVER['PHP_SELF'];
+// echo $_SERVER['SERVER_NAME'];
+// echo $_SERVER['HTTP_HOST'];
+// echo $_SERVER['HTTP_REFERER'];
+// echo $_SERVER['HTTP_USER_AGENT'];
+// echo $_SERVER['SCRIPT_NAME'];
+
+//  $_REQUEST is an array containing data from $_GET, $_POST, and $_COOKIE.
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_REQUEST['fname']);
+    if (empty($name)) {
+      echo "Name is empty";
+    } else {
+      echo $name;
+    }
+  }
 ?>
+</body>
+</html>
