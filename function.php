4e87fc84a6bcdaeb420e192423adc625dd2d7776
Lincolin Angaya <lincolinangaya5626@gmail.com>
2024-12-21 08:02:27 -0800

    functions with return values

--- function.php
+++ function.php
@@ -5,16 +5,30 @@
   
 //   myMessage();
 
+
+
 // function familyName($fname, $year){
 //     echo "$fname lincolin.<br>";
 //     echo "$year yers old";
 
 // }
 // familyName("Angaya", "21");
-function setHeight ($minHeight = 45){
 
-echo "the height is  : $minHeight <br>";
+
+// function setHeight ($minHeight = 45){
+
+// echo "the height is  : $minHeight <br>";
+
+// }
+// setHeight(25);
+
+
+function sum($x, $y){
+    $z = $x + $y;
+     return $z;
+
 
 }
-setHeight(25);
+echo sum(4,5);
+
 ?>
