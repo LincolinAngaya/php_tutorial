17cb53ebc589c68b72b6989e9ac689288f65e45e
Lincolin Angaya <lincolinangaya5626@gmail.com>
2024-12-21 07:49:41 -0800

    defaul value arguement in a php arguement

--- function.php
+++ function.php
@@ -5,10 +5,16 @@
   
 //   myMessage();
 
-function familyName($fname, $year){
-    echo "$fname lincolin.<br>";
-    echo "$year yers old";
+// function familyName($fname, $year){
+//     echo "$fname lincolin.<br>";
+//     echo "$year yers old";
+
+// }
+// familyName("Angaya", "21");
+function setHeight ($minHeight = 45){
+
+echo "the height is  : $minHeight <br>";
 
 }
-familyName("Angaya", "21");
+setHeight(25);
 ?>
