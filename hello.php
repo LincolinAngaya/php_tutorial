78e383dfa0571fb08e75361b7d4837e8083655e2
Lincolin Angaya <lincolinangaya5626@gmail.com>
2024-12-21 07:40:36 -0800

    learn a simple function program in php

--- hello.php
+++ hello.php
@@ -1,18 +1,18 @@
 <?php
 // echo 'hello world';
-$conctact = array('name', 'email', 'address');
-$variable_name = 'hello, world';
-$number = 22/2;
-echo $variable_name;
-echo $number;
-echo 'my age is $number';
+// $conctact = array('name', 'email', 'address');
+// $variable_name = 'hello, world';
+// $number = 22/2;
+// echo $variable_name;
+// echo $number;
+// echo 'my age is $number';
 
-// arrays
+// // arrays
 
 //  print_r($conctact);
 // echo $contact[0]; 
 // echo $contact[0];
-$cars = array("Volvo", "BMW", "Toyota");
+// $cars = array("Volvo", "BMW", "Toyota");
 // Indexed arrays - Arrays with a numeric index
 // Associative arrays - Arrays with named keys
 // Multidimensional arrays - Arrays containing one or more arrays
@@ -28,7 +28,23 @@ $cars = array("Volvo", "BMW", "Toyota");
 // foreach ($cars as $x) {
 //   echo "$x <br>";
 // }
-$car = array("brand"=>"Ford", "model"=>"Mustang", "year"=>1964);
-$car["year"] = 2024;
-var_dump($car);
+// $car = array("brand"=>"Ford", "model"=>"Mustang", "year"=>1964);
+// $car["year"] = 2024;
+// var_dump($car);
+
+
+// global variables/superglobal variable include
+// $GLOBALS
+// $_SERVER
+// $_REQUEST
+// $_POST
+// $_GET
+// $_FILES
+// $_ENV
+// $_COOKIE
+// $_SESSION
+// functions 
+// A function is a block of statements that can be used repeatedly in a program.
+
+
 ?>
